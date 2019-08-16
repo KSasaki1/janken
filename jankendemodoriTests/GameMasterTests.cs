ef21dd1ff284963a85b386a352f371b50445506d
KSasaki1 <53255392+KSasaki1@users.noreply.github.com>
2019-08-16 17:34:17 +0900

    Revert "Revert "プロジェクト ファイルを追加します。""
    This reverts commit 425fb8bce81d4d75a873bb7ff37a02d7069f2b66.

---
+++ jankendemodoriTests/GameMasterTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Janken;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Janken.Tests
+{
+    [TestClass()]
+    public class GameMasterTests
+    {
+        [TestMethod()]
+        public void MasterTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
